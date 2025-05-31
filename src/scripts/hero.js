5c6fd6f21c1ba9a226c5229bbeadeef8883bf32a
Alex-byteai <20223759@aloe.ulima.edu.pe>
2025-05-30 20:53:32 -0500

    Refactor Header, Hero, SectionContainer, and Layout components; enhance styles and animations

--- src/scripts/hero.js
+++ src/scripts/hero.js
@@ -1,20 +1,36 @@
 const text = document.querySelector(".hero-title");
+const text2 = document.querySelector(".hero-description");
 
 const textConvertedToArray = text.textContent.split("");
+const text2ConvertedToArray = text2.textContent.split("");
 
 let letterIndex = 0;
+let letterIndex2 = 0;
+
 let junctionOfLetters = '';
+let junctionOfLetters2 = '';
 
 text.innerText = "";
+text2.innerText = "";
 
-const iterationText = setInterval(() => {
-  if (letterIndex < textConvertedToArray.length) {
-    junctionOfLetters += textConvertedToArray[letterIndex];
-    text.innerText = junctionOfLetters;
-    letterIndex++;
-  } else {
-    clearInterval(iterationText);
-  }
-}, 100);
+const animateText = (element, arr, intervalTime, callback) => {
+  let index = 0;
+  let junction = '';
 
-console.log(textConvertedToArray);
+  const interval = setInterval(() => {
+    if(index < arr.length) {
+      junction += arr[index];
+      element.innerText = junction;
+      index++;
+    } else {
+      clearInterval(interval);
+      if (callback) callback();  
+    }
+  }, intervalTime); 
+};
+
+
+animateText(text, textConvertedToArray, 100, () => {
+  animateText(text2, text2ConvertedToArray, 30);
+});
+
